ecff316f87b9c705766b713934eff378ddb0c5a8
cryptolake <dhiadah@gmail.com>
2022-03-08 21:26:13 +0100

    added tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,23 +9,26 @@ from models.engine.file_storage import FileStorage
 
 class TestFileStorage(unittest.TestCase):
 
+    def test_save(self):
+        a = BaseModel()
+        a.save()
+        self.assertTrue(len(storage.all()) > 0)
+
     def test_file_path(self):
         self.assertIsInstance(storage._FileStorage__file_path, str)
         self.assertTrue(len(storage._FileStorage__file_path) > 0)
 
     def test_objects(self):
-        self.assertIsInstance(storage.all(), dict)
-        self.assertTrue(len(storage.all()) > 0)
+        a = BaseModel()
+        a.save()
+        self.assertIsInstance(storage._FileStorage__objects, dict)
+        self.assertTrue(len(storage._FileStorage__objects) > 0)
     
     def test_all(self):
         a = BaseModel()
         a.save()
         self.assertTrue(len(storage.all()) > 0)
 
-    def test_save(self):
-        a = BaseModel()
-        a.save()
-        self.assertTrue(len(storage.all()) > 0)
 
 if __name__ == '__main__':
     unittest.main()
